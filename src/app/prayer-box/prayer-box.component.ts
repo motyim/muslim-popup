84e884c3ff985289b589dc6ca74fe6403852a348
Mohamed MotYim <mohamed.motyim@gmail.com>
2020-10-26 14:04:18 +0200

    add - date for day

--- src/app/prayer-box/prayer-box.component.ts
+++ src/app/prayer-box/prayer-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-prayer-box',
@@ -7,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrayerBoxComponent implements OnInit {
 
-  constructor() { }
+  arabicDate = new Intl.DateTimeFormat('ar-EG-u-ca-islamic', {
+    day: 'numeric',
+    month: 'long',
+    weekday: 'long',
+    year: 'numeric'
+  }).format(Date.now());
+  engDate = new Intl.DateTimeFormat('ar-EG', {day: 'numeric', month: 'long'}).format(Date.now());
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
