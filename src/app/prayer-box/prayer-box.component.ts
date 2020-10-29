c8966a650aac0df4ab468f5e18fb6836b36ba486
Mohamed MotYim <mohamed.motyim@gmail.com>
2020-10-29 14:39:24 +0200

    related #7 add - holiday bar add - about view

--- src/app/prayer-box/prayer-box.component.ts
+++ src/app/prayer-box/prayer-box.component.ts
@@ -3,6 +3,7 @@ import {PrayerService} from '../services/prayer.service';
 import {Pray} from '../model/pray';
 import {CountdownEvent} from 'ngx-countdown';
 import {NGXLogger} from 'ngx-logger';
+import {faStarAndCrescent} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-prayer-box',
@@ -11,6 +12,7 @@ import {NGXLogger} from 'ngx-logger';
 })
 export class PrayerBoxComponent implements OnInit {
 
+  faStarAndCrescent = faStarAndCrescent;
   arabicDate = '';
   engDate = new Intl.DateTimeFormat('ar-EG', {day: 'numeric', month: 'long'}).format(Date.now());
   prayName: string;
@@ -20,6 +22,8 @@ export class PrayerBoxComponent implements OnInit {
   prayTimeNow: boolean;
   leftTime: number;
   private marginTime: number;
+  holidays: string;
+  holidayURL: string;
 
   constructor(private prayer: PrayerService, private logger: NGXLogger) {
     this.marginTime = 5;
@@ -57,7 +61,7 @@ export class PrayerBoxComponent implements OnInit {
 
     // set remain time
     const date = new Date();
-    this.logger.debug('next day ' + nextPray.nextDay , date.getDate());
+    this.logger.debug('next day ' + nextPray.nextDay, date.getDate());
     if (nextPray.nextDay) {
       date.setDate(date.getDate() + 1);
     }
@@ -82,6 +86,8 @@ export class PrayerBoxComponent implements OnInit {
     const dayNumber = hijriData.day;
     const month = hijriData.month.ar;
     const year = hijriData.year;
+
+    this.printHolidays(hijriData.holidays);
 
     return `${day}، ${dayNumber} ${month} ${year} هـ`;
   }
@@ -142,6 +148,54 @@ export class PrayerBoxComponent implements OnInit {
       this.logger.debug(event);
       this.logger.debug(`pray time is now`);
       this.prayTimeNow = true;
+    }
+  }
+
+  printHolidays(holidays): void {
+    const holidaysInfo = {
+      'Lailat-ul-Miraj': {
+        name: 'ليله المعراج',
+        url: 'https://ar.wikipedia.org/wiki/%D8%A7%D9%84%D8%A5%D8%B3%D8%B1%D8%A7%D8%A1_%D9%88%D8%A7%D9%84%D9%85%D8%B9%D8%B1%D8%A7%D8%AC'
+      },
+      'Lailat-ul-Bara\'at': {
+        name: 'ليله البارات',
+        url: 'https://ar.wikipedia.org/wiki/%D9%84%D9%8A%D9%84%D8%A9_%D9%85%D9%86%D8%AA%D8%B5%D9%81_%D8%B4%D8%B9%D8%A8%D8%A7%D9%86'
+      },
+      '1st Day of Ramadan': {
+        name: 'اول ليله رمضان',
+        url: 'https://ar.wikipedia.org/wiki/%D8%B1%D9%85%D8%B6%D8%A7%D9%86'
+      },
+      'Lailat-ul-Qadr': {
+        name: 'ليله القدر',
+        url: 'https://ar.wikipedia.org/wiki/%D9%84%D9%8A%D9%84%D8%A9_%D8%A7%D9%84%D9%82%D8%AF%D8%B1'
+      },
+      'Eid-ul-Fitr': {
+        name: 'عيد الفطر',
+        url: 'https://ar.wikipedia.org/wiki/%D8%B9%D9%8A%D8%AF_%D8%A7%D9%84%D9%81%D8%B7%D8%B1'
+      },
+      Arafa: {
+        name: 'يوم عرفه',
+        url: 'https://ar.wikipedia.org/wiki/%D9%8A%D9%88%D9%85_%D8%B9%D8%B1%D9%81%D8%A9'
+      },
+      Ashura: {
+        name: 'يوم عاشوراء',
+        url: 'https://ar.wikipedia.org/wiki/%D8%B9%D8%A7%D8%B4%D9%88%D8%B1%D8%A7%D8%A1'
+      },
+      'Eid-ul-Adha': {
+        name: 'عيد الاضحي',
+        url: 'https://ar.wikipedia.org/wiki/%D8%B9%D9%8A%D8%AF_%D8%A7%D9%84%D8%A3%D8%B6%D8%AD%D9%89'
+      },
+      'Mawlid al-Nabi': {
+        name: 'المولد النبوي',
+        url: 'https://ar.wikipedia.org/wiki/%D8%A7%D9%84%D9%85%D9%88%D9%84%D8%AF_%D8%A7%D9%84%D9%86%D8%A8%D9%88%D9%8A'
+      },
+
+    };
+    for (const key in holidays) {
+      if (holidays[key] in holidaysInfo) {
+        this.holidays = holidaysInfo[holidays[key]].name;
+        this.holidayURL = holidaysInfo[holidays[key]].url;
+      }
     }
   }
 }
